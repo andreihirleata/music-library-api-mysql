67bde0ff0df3dfce3e54fe15edb59905aafeff9f
andreihirleata <hirleata.andrei@gmail.com>
2020-05-13 18:15:50 +0100

    added /post for creating artist entry

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+
+
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
+// const artistControllers = require('./controllers/artist');
 
 const app = express();
+app.use(express.json());
+
+app.use('/artists', artistRouter);
+// app.post('/artists', artistControllers.createArtist);
 
 app.use(express.json());
 
