86961c726f2395fc3fa9bb42a72bc171677d303c
andreihirleata <hirleata.andrei@gmail.com>
2020-05-13 20:53:10 +0100

    created GET method to get an artist by ID and error handler for when the entry doesnt exist

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -3,3 +3,20 @@ const { Artist } = require('../sequelize');
 exports.createArtist = (req, res) => {
   Artist.create(req.body).then(user => res.status(201).json(user));
 };
+
+exports.listRecords = (req, res) => {
+  Artist.findAll().then(artists =>
+    res.status(200).json(artists) );
+};
+
+exports.findById = (req, res) => {
+  const artistId = parseInt(req.params.artistId, 10);
+  Artist.findByPk(artistId).then(artist => {
+    if(!artist) {
+    res.status(404).json({ error: 'The artist could not be found.' });
+  } else {
+  res.status(200).json(artist);
+}
+  });
+};
+                              
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,7 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.createArtist);
+router.get('/', artistController.listRecords);
+router.get('/:artistId', artistController.findById);
 
 module.exports = router;
